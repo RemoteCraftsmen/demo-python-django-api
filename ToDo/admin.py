88be04e5c3d95c7bfd30c8d08f29fea347b87cb6
Peter <piotr.major@remotecraftsmen.com>
2021-07-22 19:35:48 +0200

    added new tests (index and show controllers)

--- ToDo/admin.py
+++ ToDo/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import ToDo
+from .models import Todo
 
-admin.site.register(ToDo)
+admin.site.register(Todo)
 
--- ToDo/controllers/TodoViewSet.py
+++ ToDo/controllers/TodoViewSet.py
@@ -1,6 +1,6 @@
 from rest_framework import viewsets,status
 from ToDo.serializers.TodoSerializer import TodoSerializer
-from ToDo.models import ToDo
+from ToDo.models import Todo
 from ToDo.permissions.IsOwnerOrAdmin import IsOwnerOrAdmin
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
@@ -12,13 +12,13 @@ class TodoViewSet(viewsets.ModelViewSet):
     TODO Remove or split this to separate  dirs and files;
     """
     permission_classes = [IsOwnerOrAdmin, IsAuthenticated]
-    queryset = ToDo.objects.all()
+    queryset = Todo.objects.all()
     serializer_class = TodoSerializer
 
     def get_queryset(self):
         if self.request.user.is_staff:
-            return ToDo.objects.all()
-        return ToDo.objects.filter(owner=self.request.user.id)
+            return Todo.objects.all()
+        return Todo.objects.filter(owner=self.request.user.id)
 
 
     def create(self, request):
