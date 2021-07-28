39be6dfbfa17e0d0ea6203c5035a6d669daba049
Peter <piotr.major@remotecraftsmen.com>
2021-07-28 11:57:29 +0200

    added new descriptions to swagger

--- ToDo/controllers/UserViewSet.py
+++ ToDo/controllers/UserViewSet.py
@@ -1,13 +1,18 @@
 from rest_framework import viewsets, permissions
 from ToDo.serializers import UserSerializer
 from django.contrib.auth.models import User
+from drf_spectacular.utils import extend_schema_view
+from ToDo.swagger.schemas.UsersSchema import UsersSchema
 
 
+@extend_schema_view(
+    list=UsersSchema.list,
+    retrieve=UsersSchema.retrieve,
+    create=UsersSchema.create,
+    destroy=UsersSchema.destroy,
+    update=UsersSchema.update)
 class UserViewSet(viewsets.ModelViewSet):
-    """
-    Returns List of all users
-    TODO Remove or split this to separate  dirs and files; add only for admin
-    """
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [permissions.IsAdminUser]
+    http_method_names = ['get', 'post', 'head', 'put', 'delete']
