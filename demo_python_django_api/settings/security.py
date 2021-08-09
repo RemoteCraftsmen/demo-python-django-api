00ca7b5f637bcec83f0b83a752d69fa0141ab67a
piotr-major-rmtcfm-com <80816815+piotr-major-rmtcfm-com@users.noreply.github.com>
2021-08-09 13:52:37 +0200

    new envs (#25)
    Co-authored-by: Piotr Major <piotr.major@RemoteCraftsmen.local>

--- demo_python_django_api/settings/security.py
+++ demo_python_django_api/settings/security.py
@@ -1,10 +1,11 @@
+from distutils.util import strtobool
 from .env import env
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(strtobool(env('DEBUG'))) or False
 
 # Application definition
 AUTH_USER_MODEL = 'users.User'
--- demo_python_django_api/settings/spectacular.py
+++ demo_python_django_api/settings/spectacular.py
@@ -1,5 +1,5 @@
 SPECTACULAR_SETTINGS = {
     'TITLE': 'Django Todo API',
-    'DESCRIPTION': 'Your project description',
+    'DESCRIPTION': 'Demo of  Todo list API in Django',
     'VERSION': '1.0.0',
 }
